685931f646486a9858a522a1cf25abc6e83a6d81
user54244 <guillaume.germain.perso@gmail.com>
2025-06-29 12:10:20 +0700

    Update activity-manager.js

--- activity-manager.js
+++ activity-manager.js
@@ -180,8 +180,8 @@ class ActivityManagerCard extends LitElement {
                                   xmlns="http://www.w3.org/2000/svg"
                                   viewBox="0 0 24 24"
                               >
-                                  <path
-                                      d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"
+                                  <path 
+                                      d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z">
                                   />
                               </svg>
                           </mwc-icon-button>
