2c18f9d5b0eb1e6a356a7da3bea6ea5cc4ed5f6a
user54244 <guillaume.germain.perso@gmail.com>
2025-06-30 17:54:34 +0700

    Update activity-manager.js

--- activity-manager.js
+++ activity-manager.js
@@ -263,6 +263,21 @@ class ActivityManagerCard extends LitElement {
                     <mwc-icon-button
                         @click=${() => {
                             this.shadowRoot
+                                .querySelector(".filter-container")
+                                .show();
+                        }}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
+                            />
+                        </svg>
+                    </mwc-icon-button>
+                    <mwc-icon-button
+                        @click=${() => {
+                            this.shadowRoot
                                 .querySelector(".manage-form")
                                 .show();
                         }}
@@ -286,6 +301,9 @@ class ActivityManagerCard extends LitElement {
                             />
                         </svg>
                     </mwc-icon-button>
+                </div>
+                <div class="filter-container">
+                    <ha-textfield type="text" label="Filter activities" id="filter-activities"></ha-textfield>
                 </div>
             </div>
         `;
@@ -656,6 +674,9 @@ class ActivityManagerCard extends LitElement {
             align-items: center;
             justify-content: center;
             cursor: pointer;
+        }
+        .filter-container {
+            display: none;
         }
         .am-grid {
             display: grid;
